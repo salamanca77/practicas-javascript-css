55617f98830ffc6ff73deddaeafca873ce471dd7
jonn <joliversalamanca@gmail.com>
2023-11-21 16:19:35 -0400

    new file: 08-header-scrollY.html, modified: borrador.css, borrador.html, borrador.js

--- borrador.js
+++ borrador.js
@@ -1,1 +1,9 @@
-    
+const header = document.querySelector("#idHeader");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.style.backgroundColor = "green";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
